328f626f00d5ef430ba6e809e4785effe1279c84
Seremis <mauricemeedendorp@hetnet.nl>
2015-03-03 22:11:00 +0100

    The beginnings of the ultimate animation system! :X

---
+++ src/main/java/seremis/geninfusion/api/soul/IAnimation.java
@@ -0,0 +1,23 @@
+package seremis.geninfusion.api.soul;
+
+public interface IAnimation {
+
+    /**
+     * If this animation applies to this entity. This only gets called in the entity's first render tick!
+     * @param entity the entity to be animated
+     * @return true if the entity should be animated
+     */
+    public boolean shouldAnimate(IEntitySoulCustom entity);
+
+    /**
+     * Animate this entity.
+     * @param entity the entity to be animated
+     * @param timeModifier Increases over time. Has the same value as the first parameter in the setRotationAngles method in the Model class.
+     * @param walkSpeed Increases and decreases with the entity speed. (Not sure about the unit of this value). Has he same value as the 2nd parameter in setRotationAngles.
+     * @param specialRotation The rotation set in the handleRotationFloat method in the render file. Has the same value as the 3rd parameter in setRotationAngles.
+     * @param rotationYawHead The rotationYaw of the entity's head. Has the same value as the 4th parameter in setRotationAngles.
+     * @param rotationPitch The rotationPitch of the entity. Has the same value as the 5th parameter in setRotationAngles.
+     * @param scale The scale this entity should render at. Standard at 0.0625F (1/16). Has the same value as the 6th parameter in setRotationAngles.
+     */
+    public void animate(IEntitySoulCustom entity, float timeModifier, float walkSpeed, float specialRotation, float rotationYawHead, float rotationPitch, float scale);
+}
