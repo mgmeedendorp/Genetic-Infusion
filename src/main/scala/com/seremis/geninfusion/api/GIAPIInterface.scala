db78960d9ed8edccd159d4506fc387047e23735f
Seremis <Seremis@users.noreply.github.com>
2016-06-06 18:23:46 +0200

    Commit the missing file

--- src/main/scala/com/seremis/geninfusion/api/GIAPIInterface.scala
+++ src/main/scala/com/seremis/geninfusion/api/GIAPIInterface.scala
@@ -3,13 +3,18 @@ package com.seremis.geninfusion.api
 import com.seremis.geninfusion.api.genetics.{IChromosome, IGene, ISoul}
 import com.seremis.geninfusion.api.model.animation.IAnimation
 import com.seremis.geninfusion.api.soulentity.IEntityMethod
-import com.seremis.geninfusion.api.util.{DataType, TypedName}
+import com.seremis.geninfusion.api.util.{DataType, FunctionName, GeneName}
 import net.minecraft.entity.EntityLiving
 import net.minecraft.nbt.NBTTagCompound
 
 import scala.collection.mutable.ListBuffer
 
+/**
+  * This is the main place to interact with Genetic Infusion, this defines all registries in the mod.
+  */
 object GIApiInterface {
+
+    var GIModId: String = _
 
     var animationRegistry: IAnimationRegistry = _
     var dataTypeRegistry: IDataTypeRegistry = _
@@ -40,25 +45,25 @@ object GIApiInterface {
     }
 
     trait IEntityMethodRegistry {
-        def register[A](name: TypedName[A], method: IEntityMethod[A])
+        def register[A](name: FunctionName[A], method: IEntityMethod[A])
 
         @throws[IllegalArgumentException]
-        def getMethodsForName[A](name: TypedName[A]): List[IEntityMethod[A]]
-        def hasMethodForName(name: TypedName[_]): Boolean
+        def getMethodsForName[A](name: FunctionName[A]): List[IEntityMethod[A]]
+        def hasMethodForName(name: FunctionName[_]): Boolean
 
-        def getAllMethodNames: Array[TypedName[_]]
-        def getAllMethods: Map[TypedName[_], ListBuffer[IEntityMethod[_]]]
+        def getAllMethodNames: Array[FunctionName[_]]
+        def getAllMethods: Map[FunctionName[_], ListBuffer[IEntityMethod[_]]]
 
-        def getMethodsForSoul(soul: ISoul): Map[TypedName[_], List[IEntityMethod[_]]]
+        def getMethodsForSoul(soul: ISoul): Map[FunctionName[_], List[IEntityMethod[_]]]
     }
 
     trait IGeneDefaultsRegistry {
-        def register[A](clzz: Class[_ <: EntityLiving], geneName: TypedName[A], defaultValue: IChromosome[A])
+        def register[A](clzz: Class[_ <: EntityLiving], geneName: GeneName[A], defaultValue: IChromosome[A])
 
         def isClassRegistered(clzz: Class[_ <: EntityLiving]): Boolean
 
         @throws[IllegalArgumentException]
-        def getDefaultValueForClass[A](clzz: Class[_ <: EntityLiving], geneName: TypedName[A]): IChromosome[A]
+        def getDefaultValueForClass[A](clzz: Class[_ <: EntityLiving], geneName: GeneName[A]): IChromosome[A]
 
         @throws[IllegalArgumentException]
         def getSoulForClass(clzz: Class[_ <: EntityLiving]): ISoul
@@ -66,11 +71,11 @@ object GIApiInterface {
 
     trait IGeneRegistry {
         def register(gene: IGene[_])
-        def register[A](name: TypedName[A], defaultValue: IChromosome[A])
+        def register[A](name: GeneName[A], defaultValue: IChromosome[A])
 
         @throws[IllegalArgumentException]
-        def getGeneForName[A](name: TypedName[A]): IGene[A]
-        def hasGeneForName(defaultValue: TypedName[_]): Boolean
+        def getGeneForName[A](name: GeneName[A]): IGene[A]
+        def hasGeneForName(defaultValue: GeneName[_]): Boolean
 
         def getAllGenes: Array[IGene[_]]
     }
